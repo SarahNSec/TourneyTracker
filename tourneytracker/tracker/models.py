5566b27b50c54aec169c6b29521989a088066724
Sarah Noles Muth <22377256+SarahNSec@users.noreply.github.com>
2024-03-04 20:23:03 -0600

    start models

--- tourneytracker/tracker/models.py
+++ tourneytracker/tracker/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+    player_name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.player_name
+
+class Tournament(models.Model):
+    tourney_name = models.CharField("Tournament Name", max_length=256)
+    active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.tourney_name
