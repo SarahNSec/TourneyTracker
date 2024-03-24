45ee2dc7db50d140eae678710e37c18b4dacf810
Sarah Noles Muth <22377256+SarahNSec@users.noreply.github.com>
2024-03-24 13:07:19 -0500

    add import_export config to admin.py

--- tourneytracker/tracker/admin.py
+++ tourneytracker/tracker/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Player, Tournament, Division, Division_Type, Team, Tournament_Locations, Status, Outcome, Court, Location, Match
+from import_export import resources
 
 # Register your models here.
 admin.site.register(Player)
@@ -13,3 +14,9 @@ admin.site.register(Outcome)
 admin.site.register(Court)
 admin.site.register(Location)
 admin.site.register(Match)
+
+# Configure Import_Export models
+class MatchResource(resources.ModelResource):
+
+    class Meta:
+        model = Match
