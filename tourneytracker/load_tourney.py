af67ae74f1c29600e7823d46eeb589111fb08dfe
Sarah Noles Muth <22377256+SarahNSec@users.noreply.github.com>
2024-03-24 12:56:20 -0500

    Current status

--- tourneytracker/load_tourney.py
+++ tourneytracker/load_tourney.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
-# from tracker.models import Match
+from tracker.models import Match
 
-matches = pd.read_csv("./tourneytracker/data/test_tourney.csv")
+matches = pd.read_csv("./data/test_tourney.csv")
 matches = matches.replace({np.nan: None})
 
 for index, match in matches.iterrows():
