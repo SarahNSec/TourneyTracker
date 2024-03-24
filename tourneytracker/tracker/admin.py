363962d09b480a8b8f507d4b12e489cd10030222
Sarah Noles Muth <22377256+SarahNSec@users.noreply.github.com>
2024-03-24 13:11:00 -0500

    add admin interface integration

--- tourneytracker/tracker/admin.py
+++ tourneytracker/tracker/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Player, Tournament, Division, Division_Type, Team, Tournament_Locations, Status, Outcome, Court, Location, Match
 from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+# Configure Import_Export models
+class MatchResource(resources.ModelResource):
+
+    class Meta:
+        model = Match
+
+class MatchAdmin(ImportExportModelAdmin):
+    resource_classes = [MatchResource]
 
 # Register your models here.
 admin.site.register(Player)
@@ -13,10 +23,4 @@ admin.site.register(Status)
 admin.site.register(Outcome)
 admin.site.register(Court)
 admin.site.register(Location)
-admin.site.register(Match)
-
-# Configure Import_Export models
-class MatchResource(resources.ModelResource):
-
-    class Meta:
-        model = Match
+admin.site.register(Match, MatchAdmin)
