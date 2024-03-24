eaa4fff8130b795f2bd3e6da3bf356b8e27bff0a
Sarah Noles Muth <22377256+SarahNSec@users.noreply.github.com>
2024-03-24 11:27:38 -0500

    iterate through the matches

--- tourneytracker/load_tourney.py
+++ tourneytracker/load_tourney.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
+# from tracker.models import Match
 
-matches = pd.read_csv("test_tourney.csv")
+matches = pd.read_csv("./tourneytracker/data/test_tourney.csv")
 matches = matches.replace({np.nan: None})
+
+for index, match in matches.iterrows():
+    print(match)
 
 print("end")
