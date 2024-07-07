22521dc34164254f92f4ee5d524f7bf58c53d4cc
Sarah Noles Muth <22377256+SarahNSec@users.noreply.github.com>
2024-07-07 14:37:43 -0500

    build out helper function for loading data

--- tourneytracker/tracker/utils.py
+++ tourneytracker/tracker/utils.py
@@ -1,3 +1,16 @@
-def load_tourney_data(csv_data):
-    print('in tourney data function')
+from .models import Player, Tournament
+
+def load_tourney_data(file, tourney_name):
+    # First, make sure the tournament is created
+    tourney_obj, created = Tournament.objects.get_or_create(
+        tourney_name=tourney_name,
+        active=True,
+    )
+    if created is False:
+        return False
+
+    # once the tourney is created, add each of the matches, including 
+    # their corresponding objects in other models using the helper functions below.
+    for raw_match in file:
+        print(raw_match)
     return None
