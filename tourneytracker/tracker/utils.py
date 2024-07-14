d461febe949b4a0a8e1c8860601b6d1ca556d83e
Sarah Noles Muth <22377256+SarahNSec@users.noreply.github.com>
2024-07-14 14:00:53 -0500

    build out logic to create match record

--- tourneytracker/tracker/utils.py
+++ tourneytracker/tracker/utils.py
@@ -1,4 +1,4 @@
-from .models import Player, Tournament
+from .models import Tournament, Team, Location, Match, Division
 import csv
 import pandas as pd
 
@@ -13,5 +13,27 @@ def load_tourney_data(file, tourney_name):
     # their corresponding objects in other models using the helper functions below.
     df_file = pd.read_csv(file)
     for index, match in df_file.iterrows():
-        print(match)
+        # Get the match details
+        team_obj = Team.objects.get(team_name=match['team'])
+        location_obj = Location.objects.get(abbreviation=match['location'])
+        start_time = match['scheduled_start_time']
+        status = match['status']
+        r2_id = match['r2_id']
+        win_r2_id = match['win_r2_id']
+        loss_r2_id = match['loss_r2_id']
+        division = Division.objects.get_or_create(div_name=match['division'], tournament=tourney_obj)
+
+        # create the match record
+        match_obj = Match(
+            team=team_obj,
+            location=location_obj,
+            scheduled_start_time=start_time,
+            status=status,
+            r2_id=r2_id,
+            win_r2_id=win_r2_id,
+            loss_r2_id=loss_r2_id,
+            division=division
+        )
+        match_obj.save()
+        
     return None
